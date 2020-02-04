671028bd3a25f864d3a6408a27542c70e2a80c2e
GGT <atharsfake@gmail.com>
2020-02-04 17:38:13 +0800

    更新顏色

--- js/train.js
+++ js/train.js
@@ -265,15 +265,15 @@ $('.infAll').click(function(){
     $('.listView ul li.carInf').css('display','flex');
 });
 $('.infSpeed1').click(function(){
-    $('.listView ul li').siblings().hide();
+    $('.listView ul li').hide();
     $('.listView ul li.speed1').css('display','flex');
 });
 $('.infSpeed3').click(function(){
-    $('.listView ul li').siblings().hide();
+    $('.listView ul li').hide();
     $('.listView ul li.speed3').css('display','flex');
 });
 $('.infSpeed4').click(function(){
-    $('.listView ul li').siblings().hide();
+    $('.listView ul li').hide();
     $('.listView ul li.speed4').css('display','flex');
 });
 
