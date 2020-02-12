c5b182abc3d9467aa3f49b661c899c829503dd6b
GGT <atharsfake@gmail.com>
2020-02-12 10:47:51 +0800

    更新
    測試存取位置

--- js/train.js
+++ js/train.js
@@ -271,3 +271,24 @@ $('.infSpeed4').click(function(){
     $('.listView ul li.speed4').css('display','flex');
 });
 
+// 手機定位
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+  };
+  
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
